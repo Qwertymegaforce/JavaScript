c83a9cf41786d0d4a4bb04bc073890dd7c9b8c81
Qwertymegaforce <115457802+Qwertymegaforce@users.noreply.github.com>
2023-09-29 10:57:19 +0300

    Delete webpack.mixin.js

--- webpack.mixin.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-module.exports =  {
-  entry: path.resolve(__dirname, 'some.js'),
-  output: {
-    path: path.resolve(__dirname, ''),
-    filename: 'bundle.js'
-  },
-}
